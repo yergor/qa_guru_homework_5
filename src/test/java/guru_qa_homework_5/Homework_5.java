8a260fa4135224af2682862ec70c85eaec578e92
g.ericyan <yergor@mail.ru>
2022-09-09 23:18:44 +0500

    edit shouldHaveJUnit5 method.

--- src/test/java/guru_qa_homework_5/Homework_5.java
+++ src/test/java/guru_qa_homework_5/Homework_5.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Homework_5 {
@@ -13,7 +14,10 @@ public class Homework_5 {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $("#wiki-body").shouldHave(text("Soft assertions"));
-        $(byLinkText("Soft assertions")).click();
+        $(byText("Show 2 more pages…")).click();
+        $(byText("SoftAssertions")).click();
+        // $(byLinkText("Soft assertions")).click(); не уверен,
+        // что используя данную функцию кликаю на SoftAssertions в нужном месте
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
